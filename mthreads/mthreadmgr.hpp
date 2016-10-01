f01a9e615b6984f78e5952ba38d528e110324faa
sxyizhiren <78667787@qq.com>
2016-10-01 08:12:46 +0800

    update dir

--- mthreads/mthreadmgr.hpp
+++ mthreads/mthreadmgr.hpp
@@ -9,11 +9,14 @@ public:
 
 	static CCmThreadManager* instance();
 
-	auto getMThreads(const std::string& threadName);
+	auto getMThreads(const std::string& threadName)
+		-> shared_ptr<MThreads>;
 
-	auto createMThreads(const std::string& threadName, size_t  size= 1);
+	auto createMThreads(const std::string& threadName, size_t  size= 1)
+		-> shared_ptr<MThreads>;
 
-	auto getCurrentMThreads();
+	auto getCurrentMThreads()
+		-> shared_ptr<MThreads>;
 
 public:
 	
